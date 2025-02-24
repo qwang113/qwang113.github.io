d502cefb6dc115333939e758cb2dec63ab70f860
Qi Wang <1061673783@qq.com>
2025-02-24 15:37:18 -0800

    1

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -2,7 +2,7 @@
 
 const content_dir = 'contents/'
 const config_file = 'config.yml'
-const section_names = ['home', 'publications', 'teaching']
+const section_names = ['home','profile', 'publications', 'teaching','Talks']
 
 
 window.addEventListener('DOMContentLoaded', event => {
